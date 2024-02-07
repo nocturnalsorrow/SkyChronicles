a358d90fc18daf7a32a2af29e08a35b37ceb548e
Danial Rehman <danial.rehman36@gmail.com>
2024-02-07 19:19:27 +0200

    Added create, update and delete articles functionality by ADMIN.

--- src/main/java/com/dr/skychronicles/controller/ArticleController.java
+++ src/main/java/com/dr/skychronicles/controller/ArticleController.java
@@ -3,11 +3,12 @@ package com.dr.skychronicles.controller;
 import com.dr.skychronicles.entity.Article;
 import com.dr.skychronicles.service.ArticleService;
 import com.dr.skychronicles.service.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Optional;
 
@@ -22,7 +23,7 @@ public class ArticleController {
     }
 
     @GetMapping("/article/{articleId}")
-    public String getArticle(@PathVariable Long articleId, Model model) {
+    public String getArticlePage(@PathVariable Long articleId, Model model) {
         Optional<Article> optionalArticle = articleService.getArticleById(articleId);
 
         if (optionalArticle.isPresent()) {
@@ -34,4 +35,50 @@ public class ArticleController {
         return "article";
     }
 
+    @GetMapping("/articles")
+    public String getAllArticles(Model model) {
+        model.addAttribute("articles", articleService.getArticlesSortedByDate());
+
+        return "articles";
+    }
+
+    @GetMapping("/article/update/{articleId}")
+    public String getArticleToUpdate(@PathVariable Long articleId, Model model) {
+        Optional<Article> optionalArticle = articleService.getArticleById(articleId);
+        if (optionalArticle.isPresent()) {
+            model.addAttribute("article", optionalArticle.get());
+        } else {
+            model.addAttribute("article", null);
+        }
+        return "updateArticle";
+    }
+
+    @PostMapping("/article/update")
+    public String updateArticle(@ModelAttribute Article updatedArticle) {
+        articleService.updateArticle(updatedArticle);
+
+        return "redirect:/articles";
+    }
+
+    @GetMapping ("/article")
+    public String getArticleToCreate(Model model) {
+        model.addAttribute("article", new Article());
+        model.addAttribute("categories", categoryService.getAllCategories());
+
+        return "createArticle";
+    }
+
+    @PostMapping("/article")
+    public String createArticle(@ModelAttribute Article article) {
+        articleService.createArticle(article);
+
+        return "redirect:/articles";
+    }
+
+    @GetMapping("/article/delete/{articleId}")
+    public String deleteArticle(@PathVariable Long articleId) {
+        articleService.deleteArticleById(articleId);
+
+        return "redirect:/articles";
+    }
 }
--- src/main/java/com/dr/skychronicles/controller/ProfileController.java
+++ src/main/java/com/dr/skychronicles/controller/ProfileController.java
@@ -1,8 +1,35 @@
 package com.dr.skychronicles.controller;
 
+import com.dr.skychronicles.entity.User;
+import com.dr.skychronicles.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProfileController {
+    private  final UserService userService;
 
+    @Autowired
+    public ProfileController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/profile")
+    public String getProfile(Authentication authentication, Model model) {
+        model.addAttribute("user", userService.getUserByEmail(authentication.getName()));
+
+        return "profile";
+    }
+
+    @PostMapping("/profile/edit")
+    public String edit(@ModelAttribute User updatedUser, Authentication authentication){
+        userService.updateUser(updatedUser, authentication);
+
+        return "redirect:/profile";
+    }
 }
