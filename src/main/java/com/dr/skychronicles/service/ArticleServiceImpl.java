48e3b7308aa29bf46ea3fcba606b21f414e9b731
Danial Rehman <danial.rehman36@gmail.com>
2024-02-22 18:28:26 +0200

    Updated saveArticle method in ArticleServiceImpl.java class/

--- src/main/java/com/dr/skychronicles/service/ArticleServiceImpl.java
+++ src/main/java/com/dr/skychronicles/service/ArticleServiceImpl.java
@@ -54,16 +54,24 @@ public class ArticleServiceImpl implements ArticleService {
 
     @Override
     public Article saveArticle(Article article, List<MultipartFile> imageFiles) throws IOException {
-        List<Gallery> articleImages = new ArrayList<>();
-        for (MultipartFile file : imageFiles) {
-            Gallery articleImage = new Gallery();
-            articleImage.setImage(Base64.getEncoder().encodeToString(file.getBytes()));
-            articleImage.setArticle(article);
-            articleImages.add(articleImage);
+        if (imageFiles != null && !imageFiles.isEmpty()) {
+            List<Gallery> articleImages = new ArrayList<>();
+            for (MultipartFile file : imageFiles) {
+                if (file.getSize() > 0) { // Проверка, что файл не пуст
+                    Gallery articleImage = new Gallery();
+                    articleImage.setImage(Base64.getEncoder().encodeToString(file.getBytes()));
+                    articleImage.setArticle(article);
+                    articleImages.add(articleImage);
+                }
+            }
+            article.setImages(articleImages);
+        } else {
+            // Если fileImages не содержит фотографий, присвоить ему null
+            article.setImages(null);
         }
-        article.setImages(articleImages);
         return articleRepository.save(article);
     }
+
 
     @Override
     public Optional<Article> getArticleById(Long id) {
