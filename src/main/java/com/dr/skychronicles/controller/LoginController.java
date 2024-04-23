4aa19116847c84c8b618469fbc505b2a77930946
Danial Rehman <danial.rehman36@gmail.com>
2024-04-23 17:47:53 +0300

    Design changes

--- src/main/java/com/dr/skychronicles/controller/LoginController.java
+++ src/main/java/com/dr/skychronicles/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.dr.skychronicles.controller;
 
+import com.dr.skychronicles.service.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +9,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    private final CategoryService categoryService;
+
+    public LoginController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @GetMapping("/login")
     public String login(@RequestParam(required = false) String error, Model model) {
         if (error != null) {
             model.addAttribute("error", "Invalid email or password. Please try again.");
         }
+        model.addAttribute("categories", categoryService.getAllCategories());
         return "login";
     }
 }
--- src/main/java/com/dr/skychronicles/controller/SignUpController.java
+++ src/main/java/com/dr/skychronicles/controller/SignUpController.java
@@ -1,6 +1,7 @@
 package com.dr.skychronicles.controller;
 
 import com.dr.skychronicles.entity.User;
+import com.dr.skychronicles.service.CategoryService;
 import com.dr.skychronicles.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,15 +12,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class SignUpController {
     private final UserService userService;
+    private final CategoryService  categoryService;
 
-    public SignUpController(UserService userService) {
+    public SignUpController(UserService userService, CategoryService categoryService) {
         this.userService = userService;
+        this.categoryService = categoryService;
     }
 
     @GetMapping("/signUp")
     public String signUpForm(Model model){
         model.addAttribute("user", new User());
-
+        model.addAttribute("categories", categoryService.getAllCategories());
         return "signUp";
     }
 
